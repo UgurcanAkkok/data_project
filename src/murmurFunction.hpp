b6518d1b884d7110481c134102d659d0a5fc4017
Uğurcan Akkök <ucak_gestalt@protonmail.com>
2019-12-26 15:42:02 +0300

    Did forgot to add murmur header

---
+++ src/murmurFunction.hpp
@@ -0,0 +1,9 @@
+#ifndef MURMUR3_HASH_FUNCTION_HPP
+#define MURMUR3_HASH_FUNCTION_HPP
+
+//TODO: Support MS-VS
+
+#include <stdint.h>
+void murmur(const void* key, int len, uint32_t seed, uint64_t out[2]);
+
+#endif
