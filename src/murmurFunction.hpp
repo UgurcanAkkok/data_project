dc3858810a63325f3e9327c3fb6f1a8bf42067ef
Uğurcan Akkök <ucak_gestalt@protonmail.com>
2019-12-26 16:56:29 +0300

    nothin

--- src/murmurFunction.hpp
+++ src/murmurFunction.hpp
@@ -1,9 +1,8 @@
 #ifndef MURMUR3_HASH_FUNCTION_HPP
 #define MURMUR3_HASH_FUNCTION_HPP
 
-//TODO: Support MS-VS
-
 #include <stdint.h>
+
 void murmur(const void* key, int len, uint32_t seed, uint64_t out[2]);
 
 #endif
